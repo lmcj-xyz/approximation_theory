5221778d04d9951335a0998e651aac1e50942029
LM UoL laptop <mmlmcj@leeds.ac.uk>
2023-02-15 12:58:59 +0000

    Chapter 2 done. Changing to Jupyter Notebooks

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Approximation-Theory
  Following the book "Approximation Theory and Approximation Practice" by Trefethen in Numpy Python instead of Matlab.
  
- Not following the work from [chebpy](https://github.com/chebpy/chebpy) since my aim is to use standard NumPy instead.
- It looks fun though.
+ Doing it on Jupyter Notebooks to follow the spirit of the book.
+ 
+ Not using neither
+ [pychebfun](https://github.com/olivierverdier/pychebfun)
+ nor
+ [chebpy](https://github.com/chebpy/chebpy)
+ since my aim is to use standard NumPy instead.
+ They fun though.
--- py_scripts/C2_Chebyshev_Points_and_Interpolators.py
+++ py_scripts/C2_Chebyshev_Points_and_Interpolators.py
@@ -6,11 +6,9 @@ Created on Mon Feb 13 12:46:11 2023
 @title: Chebyshev points and Interpolants
 """
 
-#%%
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
 n = 16
 
 # Equally spaced angles theta_j
@@ -19,56 +17,52 @@ tt = np.linspace(0, np.pi, n+1)
 # Points on the upper half of the unit circle (complex plane)
 zz = np.exp(1j*tt)
 # Extract real and imaginary part
-xx = zz.real
+xx1 = zz.real
 yy = zz.imag
 
 # Plot
 plt.figure(num="eq_p_1")
-plt.title("Equispaced points on the unit circle")
+plt.title("Equispaced points on the unit circle \n obtained by extracting real part")
 plt.xlabel("Real")
 plt.ylabel("Imaginary")
-plt.plot(xx, yy, marker='.', color='g')
+plt.plot(xx1, yy, marker='.', color='g')
 plt.show()
 
-#%%
 # Chebyshev points are the real part of the numbers zz, namely xx
 # Interm of the original angles we can define them as
 xx2 = np.cos(tt)
 
 # Plot
 plt.figure(num="eq_p_2")
-plt.title("Equispaced points on the unit circle")
+plt.title("Equispaced points on the unit circle \n obtained by computing the cos of the angles")
 plt.xlabel("Real")
 plt.ylabel("Imaginary")
 plt.plot(xx2, yy, marker='.', color='m')
 plt.show()
 
-#%%
 # NumPy can also get the Chebysev points of the second kind
 # with a built in method
 xx3 = np.polynomial.chebyshev.chebpts2(n+1)
 
 # Plot
 plt.figure(num="eq_p_3")
-plt.title("Equispaced points on the unit circle")
+plt.title("Equispaced points on the unit circle \n obtained with built in NumPy method")
 plt.xlabel("Real")
 plt.ylabel("Imaginary")
 plt.plot(xx3, yy, marker='.', color='r')
 plt.show()
 
-#%%
 # Visualizing the Chebyshev points
 
 # Plot
 plt.figure(num="eq_p_4")
-plt.title("Equispaced points on the unit circle")
+plt.title("Equispaced points on the unit circle \n visualizin Chebyshev points on the x axis")
 plt.xlabel("Real")
 plt.ylabel("Imaginary")
-plt.plot(xx, yy, marker='.')
-plt.plot(xx, np.zeros_like(xx), marker='.')
+plt.plot(xx1, yy, marker='.')
+plt.plot(xx1, np.zeros_like(xx1), marker='.')
 plt.show()
 
-#%%
 # Some points to work with
 xx = np.linspace(-1, 1, 1000)
 # Defining some functions to find their interpolators
@@ -106,4 +100,21 @@ plt.figure(num="interp_3")
 plt.title(r"Degree 100 Chebyshev interpolant for $f = \sin(6x) + sign(\sin(x + e^{2x}))$")
 plt.plot(xx, g_eval, color='r')
 plt.plot(xx, cheb_eval_g_100, color='c')
+plt.show()
+
+# Random data
+rng = np.random.default_rng(12345) # The seed is to make it reproducible
+rn_data = 2*rng.uniform(size=100) - 1
+x2 = np.linspace(start=-1, stop=1, num=100)
+# Fit Chebyshev interpolant
+cheb_coeff_random = np.polynomial.chebyshev.chebfit(x=x2, y=rn_data, deg=100)
+# Evaluate the interpolant
+cheb_fit_random = np.polynomial.chebyshev.chebval(x2, cheb_coeff_random)
+
+# Plot
+plt.figure(num="interp_4")
+plt.title("Chebyshev interpolant through random data")
+plt.scatter(x2, rn_data, color='b', marker='.', label='Random points')
+plt.plot(x2, cheb_fit_random, color='c', label='Interpolant')
+plt.legend()
 plt.show()
